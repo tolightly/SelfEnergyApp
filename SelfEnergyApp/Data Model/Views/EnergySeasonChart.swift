65a268c37ef9afa20082989870665494b13cd4ff
toLight <tolightbook@gmail.com>
2023-10-18 20:04:44 +0300

    Add Season and Year Chart

---
+++ SelfEnergyApp/Data Model/Views/EnergySeasonChart.swift
@@ -0,0 +1,18 @@
+//
+//  EnergySeasonChart.swift
+//  SelfEnergyApp
+//
+//  Created by Denys Nazymok on 18.10.2023.
+//
+
+import SwiftUI
+
+struct EnergySeasonChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnergySeasonChart()
+}
---
+++ SelfEnergyApp/Data Model/Views/EnergyYearChart.swift
@@ -0,0 +1,18 @@
+//
+//  EnergyYearChart.swift
+//  SelfEnergyApp
+//
+//  Created by Denys Nazymok on 18.10.2023.
+//
+
+import SwiftUI
+
+struct EnergyYearChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnergyYearChart()
+}
